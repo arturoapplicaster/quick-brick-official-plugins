b8608931fbb1362ab846f1e5cb8c9642cc100250
oduk <oduk@scand.com>
2020-03-26 15:11:08 +0300

    [QuickBrick] Refactored code. Bumped QB dependencies versions.

--- plugins/quick-brick-adobe-primetime-auth/src/ReactNative/AdobeComponent.js
+++ plugins/quick-brick-adobe-primetime-auth/src/ReactNative/AdobeComponent.js
@@ -1,161 +1,156 @@
 import React, { Component } from 'react';
 import {
-    NativeModules,
-    StyleSheet,
-    TouchableOpacity,
-    ActivityIndicator,
-    NativeEventEmitter,
-    View,
-    Text,
-    FlatList
+  NativeModules,
+  StyleSheet,
+  ActivityIndicator,
+  NativeEventEmitter,
+  View,
+  Text,
+  SafeAreaView
 } from 'react-native';
+import { ProvidersList } from './Components/ProvidersList.js';
+import { connectToStore } from '@applicaster/zapp-react-native-redux';
+import { isTriggerOnAppLaunch } from './Utils';
 
-import { connectToStore } from '@applicaster/zapp-react-native-redux'
-import { ProviderCell } from './ProviderCell/ProviderCell.js'
 
 const storeConnector = connectToStore(state => { //Store connector entity to obtain screen data
-    const values = Object.values(state.rivers)
+    const values = Object.values(state.rivers);
     const screenData = values.find(
-        ({ type }) => type === 'adobe-primetime-auth-qb',
-    )
+        ({ type }) => type === 'adobe-primetime-auth-qb'
+    );
     return { screenData }
-})
+});
+const adobeAccessEnabler = NativeModules.AdobeAccessEnabler; // Native module that will receive events (login, etc...)
+const adobeEventsListener = new NativeEventEmitter(adobeAccessEnabler); //Native module that will send events to RN
 
-const adobeAccessEnabler = NativeModules.AdobeAccessEnabler // Native module that will receive events (login, etc...)
-const adobeEventsListener = new NativeEventEmitter(NativeModules.AdobeAccessEnabler) //Native module that will send events to RN
 
 class AdobeComponent extends Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            loading: true,
-            dataSource: null
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      dataSource: null
     }
+  }
 
-    componentDidMount() {
-        this.setState({ loading: true });
-        const isTriggerOnAppLaunch = (navigator) => { 
-            return R.pathOr(false, ['payload', 'home'], navigator.routeData());
-        };
-        const {title, id} = this.props.payload;
-        const additionalParams = {
-            'isTriggerOnAppLaunch': isTriggerOnAppLaunch == null ? true : false,
-            'itemTitle': title,
-            'itemID': id
-        };
+  startFlow = () => {
+    const {
+      screenData = {},
+      payload = {},
+      navigator = {}
+    } = this.props;
 
-        //set Initialize AccessEnabler
-        adobeAccessEnabler.setupAccessEnabler(this.props.screenData.general);
-        adobeAccessEnabler.startLoginFlow(additionalParams,function(error, response) {
-            console.log(response)
+    const { title = '', id = '' } = payload;
+
+    const additionalParams = {
+      isTriggerOnAppLaunch: isTriggerOnAppLaunch(navigator),
+      itemTitle: title,
+      itemID: id
+    };
+
+    //set Initialize AccessEnabler
+    adobeAccessEnabler.setupAccessEnabler(screenData.general);
+    adobeAccessEnabler.startLoginFlow(additionalParams, function (error, response) {
+      console.log(response, 'response from native side');
+    });
+
+    //subscribe on update of mvpds
+    adobeEventsListener.addListener(
+      "showProvidersList",
+      (response) => {
+        this.setState({
+          loading: false,
+          dataSource: response
         });
+      }
+    )
+  };
 
-        //subscribe on update of mvpds
-        adobeEventsListener.addListener(
-            "showProvidersList",
-            res => {
-                this.setState({ loading: false })
-                this.setState({ dataSource: res })
-            }
-        )
-    }
+  componentDidMount() {
+    this.setState({ loading: true });
+    this.startFlow();
+  }
 
-    setProviderID(id) {
-        adobeAccessEnabler.setProviderID(id);
-    }
+  setProviderID = (id) => {
+    adobeAccessEnabler.setProviderID(id);
+  };
 
-    renderActivityIndicator(loadingMessage) {
-        return (
-            <View style={styles.loadingContainer}>
-                <Text style={styles.loadingTitle}>{loadingMessage}</Text>
-                <View>
-                    <ActivityIndicator style={{ marginTop: 20}}  size="large" color='white' />
-                </View>
-            </View>
-        );
-    }
-
-    renderProviderCell = ({ item }) => (
-        <TouchableOpacity onPress={() => this.setProviderID(item.id)}> 
-            <ProviderCell
-                id={item.id}
-                title={item.title}
-                logoURL={item.logoURL}
-            />
-        </TouchableOpacity>
+  renderActivityIndicator = (loadingMessage) => {
+    return (
+      <View style={styles.loadingContainer}>
+        <Text style={styles.loadingTitle}>{loadingMessage}</Text>
+        <ActivityIndicator style={{ marginTop: 20 }} size='large' color='white'/>
+      </View>
     );
+  };
 
-    renderPickerScreen(pickerScreenData) {
-        const {navigationBarBackgroundColor, navigationBarTitleColor} = pickerScreenData
-        return (
-            <View style={styles.pickerScreenContainer}>
-                <View style={{...styles.pickerScreenNavigationBar, backgroundColor: navigationBarBackgroundColor}}>
-                          <Text style={{...styles.pickerNavigationBarTitle, color: navigationBarTitleColor}}>
-                                    {pickerScreenData.navigationBarTitle}
-                          </Text>
-                </View>
-                <FlatList
-                    data={this.state.dataSource}
-                    renderItem={this.renderProviderCell}
-                />
-            </View>
-        );
-    }
+  renderPickerScreen(pickerScreenData) {
+    const {
+      navigationBarBackgroundColor,
+      navigationBarTitleColor,
+      navigationBarTitle
+    } = pickerScreenData;
 
-    render() {
-        const {
-            general: {
-                login_navbar_background_color: navigationBarBackgroundColor,
-                login_navbar_title_color: navigationBarTitleColor,
-                login_navbar_title: navigationBarTitle,
-                login_loading_message: loadingMessage,
-            } = {}
-        } = this.props.screenData || {};
+    return (
+      <SafeAreaView style={styles.pickerScreenContainer}>
+        <View style={{ ...styles.pickerScreenNavigationBar, backgroundColor: navigationBarBackgroundColor }}>
+          <Text style={{...styles.pickerNavigationBarTitle, color: navigationBarTitleColor}}>
+            {navigationBarTitle}
+          </Text>
+        </View>
+        <ProvidersList data={this.state.dataSource} setProviderID={this.setProviderID} />
+      </SafeAreaView>
+    );
+  }
 
-        const pickerScreenData = {
-            navigationBarBackgroundColor,
-            navigationBarTitleColor,
-            navigationBarTitle
-        };
+  render() {
+    const {
+      general: {
+        login_navbar_background_color: navigationBarBackgroundColor,
+        login_navbar_title_color: navigationBarTitleColor,
+        login_navbar_title: navigationBarTitle,
+        login_loading_message: loadingMessage
+      } = {}
+    } = this.props.screenData || {};
 
-        return (
-            this.state.loading ? this.renderActivityIndicator(loadingMessage) :
-                                 this.renderPickerScreen(pickerScreenData)
-        );
-    }
+    const pickerScreenData = {
+      navigationBarBackgroundColor,
+      navigationBarTitleColor,
+      navigationBarTitle
+    };
+
+    return (
+      this.state.loading
+        ? this.renderActivityIndicator(loadingMessage)
+        : this.renderPickerScreen(pickerScreenData)
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    loadingContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    loadingTitle: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: 'white'
-    },
-    pickerScreenContainer: {
-        flex: 1,
-        backgroundColor: "white"
-    },
-    pickerScreenNavigationBar: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: 70
-    },
-    pickerNavigationBarTitle: {
-        position: 'absolute',
-        bottom: 5
-    },
-    horizontal: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        paddingBottom: 5
-    }
-})
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  loadingTitle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white'
+  },
+  pickerScreenContainer: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  pickerScreenNavigationBar: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 70
+  },
+  pickerNavigationBarTitle: {
+    position: 'absolute',
+    bottom: 5
+  }
+});
 
 export default storeConnector(AdobeComponent);
